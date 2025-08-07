8c9789866a1d35c57487d151ba5ee3f25cc8aa19
mikolaj-rellify <synowim22@bonaventure.edu>
2025-08-07 14:30:25 -0400

    com

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-
+import Player from'./components/Player.jsx';
 function App() {
-  
+  return <main>
+    <div id = "game-container">
+      <ol id="players">
+        <Player name="Player 1" symbol="X" />
+        <Player name="Player 2" symbol="O" />
+      </ol>
+      </div>
 
-  return (
-    <h1>Tic-Tac-Toe</h1>
-  )
+  </main>
 }
 
 export default App
